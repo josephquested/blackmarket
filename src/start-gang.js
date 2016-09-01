511aef3339f942beeec074a09ea2e377497b3c63
josephquested <josephquested@gmail.com>
2016-09-01 16:15:43 +1200

    add confirm password section

--- src/start-gang.js
+++ src/start-gang.js
@@ -1,13 +1,14 @@
 var event = require('./event')
 var ajax = require('./ajax/ajax')
 
-var initActions = [
-  {
+var initActions = (gangName) => {
+ return [{
     type: 'password',
     name: 'password',
-    message: 'what is the password for *gang?*'
-  }
-]
+    message: `what is the password for ${gangName}?`
+  }]
+}
+
 
 var confirmPasswordActions = [
   {
@@ -17,16 +18,36 @@ var confirmPasswordActions = [
   }
 ]
 
+var confirmStartGangActions = [
+  {
+    type: 'list',
+    name: 'password',
+    message: 'are you sure you want to start the *gang*?'
+    choices: ['yes', 'no']
+  }
+]
+
 function initEvent (gangName) {
   initActions[0].message = `what is the password for ${gangName}?`
-  event(initActions, (action) => {
+  event(initActions(gangName), (action) => {
     if (action.password == '') return initEvent(gangName)
-    return confirmPasswordEvent(action.password)
+    return confirmPasswordEvent(gangName, action.password)
   })
 }
 
-function confirmPasswordEvent (password) {
+function confirmPasswordEvent (gangName, password) {
   event(confirmPasswordActions, (action) => {
+    if (action.password == '') return confirmPasswordEvent(gangName, password)
+    if (action.password == password) {
+      console.log('passwords match!')
+    } else {
+      console.log('wrong')
+    }
+  })
+}
+
+function confirmStartGang (gangName, password) {
+  event(confirmStartGangActions, (action) => {
     if (action.password == '') return confirmPasswordEvent(password)
     if (action.password == password) {
       console.log('passwords match!')
